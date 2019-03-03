e09b6f7d755babe05dec1f3b394d3937d3007a72
aillusions <aillusions@gmail.com>
2019-03-03 17:42:23 +0200

    RadioSelectorTest - wip

---
+++ js_unit/RadioSelectorTest.js
@@ -0,0 +1,12 @@
+'use strict';
+
+describe('RadioSelectorTest', function () {
+
+    it('should create RadioSelector', function (done) {
+
+        var radioSelector = new RadioSelector();
+        // debugger
+        done();
+    });
+
+});
